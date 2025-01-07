e455f653aee26c14e3decaf21803f0483f9b3e44
noshy Ayman <https://githup.com/NoshyAymanElozere>
2025-01-07 23:38:56 +0200

    aking responsive for sinup

--- components/template/SignUp/PlateFormBanner.tsx
+++ components/template/SignUp/PlateFormBanner.tsx
@@ -1,19 +1,21 @@
-import React from "react"
+import React from "react";
 
 function PlateFormBanner() {
   return (
     <div
-      className="w-full h-[250px] bg-cover bg-bottom flex item-center justify-center"
+      className="w-full patternY h-[250px] bg-cover bg-bottom flex item-center justify-center"
       style={{ backgroundImage: "url('/images/banners.png')" }}
     >
       <div className="m-auto text-center">
-        <h2 className="text-[50px] text-secondary">Join Our PLATFORM!</h2>
-        <p className="mt-3 text-2xl text-gray ">
+        <h2 className="text-3xl  md:text-[50px] text-secondary">
+          Join Our PLATFORM!
+        </h2>
+        <p className="mt-3  md:text-2xl text-gray ">
           Sign up now to start your advertising journey with us.
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default PlateFormBanner
+export default PlateFormBanner;
